8c9e2bb6bc93f8de2cf51d7874070841e317ab90
sublime392 <andrewcwinter@gmail.com>
2022-09-12 14:20:53 -0400

    2 files modified, 1 added

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -1,18 +1,69 @@
-import { Component, OnInit } from '@angular/core'
-
-import { Item } from './item'
-import { ItemService } from './item.service'
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Application, CoreTypes, Screen } from '@nativescript/core';
 
 @Component({
   selector: 'ns-items',
   templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css'],
 })
 export class ItemsComponent implements OnInit {
-  items: Array<Item>
+  @ViewChild('cameraOverlay', { static: false }) public cameraOverlay?: any;
 
-  constructor(private itemService: ItemService) {}
+  public currentOrientation = 'portrait';
 
-  ngOnInit(): void {
-    this.items = this.itemService.getItems()
+  constructor() {
+    Application.on(
+      Application.orientationChangedEvent,
+      this.handleOrientationChange,
+      this
+    );
   }
+
+  handleOrientationChange(args) {
+    console.log('ORIENTATION1:', Application.orientation());
+    setTimeout(() => {
+      this.currentOrientation = Application.orientation();
+      if (this.currentOrientation === 'portrait') {
+        this.cameraOverlay.nativeElement.animate({
+          rotate: 90,
+          duration: 200,
+          curve: CoreTypes.AnimationCurve.easeIn,
+        });
+      } else {
+        this.cameraOverlay.nativeElement.animate({
+          rotate: 0,
+          duration: 200,
+          curve: CoreTypes.AnimationCurve.easeIn,
+        });
+      }
+    }, 1000);
+
+    setTimeout(() => {
+      const deviceWidth = Screen.mainScreen.widthDIPs;
+      const deviceHeight = Screen.mainScreen.heightDIPs;
+
+      // this.widthDIPs = Math.max(Screen.mainScreen.widthDIPs, Screen.mainScreen.heightDIPs);
+      this.cameraOverlay.nativeElement.height = Math.max(
+        Screen.mainScreen.widthDIPs,
+        Screen.mainScreen.heightDIPs
+      );
+      this.cameraOverlay.nativeElement.width = Math.max(
+        Screen.mainScreen.widthDIPs,
+        Screen.mainScreen.heightDIPs
+      );
+      this.cameraOverlay.height = Math.max(
+        Screen.mainScreen.widthDIPs,
+        Screen.mainScreen.heightDIPs
+      );
+      this.cameraOverlay.width = Math.max(
+        Screen.mainScreen.widthDIPs,
+        Screen.mainScreen.heightDIPs
+      );
+    }, 2000);
+    setTimeout(() => {
+      this.cameraOverlay?.nativeElement.requestLayout();
+    }, 3000);
+  }
+
+  ngOnInit(): void {}
 }
